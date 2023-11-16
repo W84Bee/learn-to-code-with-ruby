eca080805209bde629182841b8d3a2895cb16082
Brandon Alexander <bjvonalexander@gmail.com>
2023-11-16 06:23:13 -0600

    Comments/ Multi Comments

--- 02 - Getting Started/the_p_method.rb
+++ 02 - Getting Started/the_p_method.rb
@@ -1,3 +1,4 @@
 puts "Steven Seagal"
 p "Steven Seagal"
 # prints out the full string to remind us that it is a string
+# p method shows the original code helps the developer print out technical view
