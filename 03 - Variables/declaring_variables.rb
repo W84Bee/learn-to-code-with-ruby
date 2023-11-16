22f9a8260102a5a61dfdc5abb95041143f5ad100
Brandon Alexander <bjvonalexander@gmail.com>
2023-11-16 07:18:16 -0600

    Declaration, Swap, Parallel Declaration

---
+++ 03 - Variables/declaring_variables.rb
@@ -0,0 +1,17 @@
+age = 31
+puts age
+
+name = "Boris"
+last_name = "Danger"
+puts name
+puts last_name
+
+puts age + 7 #31 + 7
+
+puts name + last_name
+
+age = 35
+puts age
+
+age = age + 10
+puts age
