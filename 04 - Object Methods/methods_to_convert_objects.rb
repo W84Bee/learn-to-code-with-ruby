2921234077b8b6783aa2f8c7a0bbae1728aecc3c
Brandon Alexander <bjvonalexander@gmail.com>
2023-11-16 08:14:18 -0600

    Object Methods Section

---
+++ 04 - Object Methods/methods_to_convert_objects.rb
@@ -0,0 +1,18 @@
+test = "5"
+puts test
+puts test.class
+puts test.to_i
+puts test.to_i.class
+puts "Apple 15".to_i
+puts test.to_f
+puts test.to_f.class
+
+puts
+
+number = 5
+puts number.class
+puts number.to_s
+puts number.to_s.class
+puts number.to_f
+puts number.to_f.class
+
---
+++ 04 - Object Methods/the_class_method.rb
@@ -0,0 +1,14 @@
+# Class - A blueprint for an object (template/schematic)
+# Object - An "instance" of the class
+
+puts "hello".class
+puts "Ruby is fun".class
+puts "1234".class
+puts "".class
+
+puts
+
+puts 5.class
+puts -23.class
+puts 300.class
+puts 3.14.class
